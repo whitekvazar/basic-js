af197e638b758f754eafe60e82a52469ae111131
Kirill Romanovskii <whitekvazar@gmail.com>
2021-02-21 18:46:49 +0300

    feat: complete createDreamTeam function

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,18 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  let resultName = [];
+
+  if (!Array.isArray(members)) {
+    return false;
+  }
+
+  members.forEach(element => {
+    if (typeof (element) == 'string') {
+      let tempStrArray = element.toUpperCase().split(' ').join('').split('');
+      resultName.push(tempStrArray[0]);
+    }
+  });
+
+  return resultName.sort().join('');
 };
