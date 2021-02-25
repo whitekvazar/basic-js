2c971b79d904c8cb4e317caba4b5c08bc1015561
Kirill Romanovskii <whitekvazar@gmail.com>
2021-02-25 16:49:53 +0300

    feat: complete repeater function

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,17 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+  let cleanStr = String(str);
+  let separator;
+  options.separator !== undefined ? separator = options.separator : separator = '+';
+  let repeatTimes = options.repeatTimes;
+
+  let addition;
+  options.addition !== undefined ? addition = String(options.addition) : addition = '';
+  let additionSeparator;
+  options.additionSeparator !== undefined ? additionSeparator = options.additionSeparator : additionSeparator = '|';
+  let additionRepeatTimes = options.additionRepeatTimes;
+  let compliteAddition = (addition + additionSeparator).repeat(additionRepeatTimes - 1) + addition;
+
+  return (cleanStr + compliteAddition + separator).repeat(repeatTimes - 1) + cleanStr + compliteAddition;
 };
-  
