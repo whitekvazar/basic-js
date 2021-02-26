9d11701d2adec1eae32462c30163a845fb7b3637
Kirill Romanovskii <whitekvazar@gmail.com>
2021-02-26 13:06:27 +0300

    feat: complete VigenereCipheringMachine class

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,13 +1,61 @@
 const CustomError = require("../extensions/custom-error");
 
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  constructor(type) {
+    this.type = type;
+  }
+
+  encrypt(message, key) {
+    if (message == undefined || key == undefined) {
+      throw new Error();
+    }
+
+    message = message.toUpperCase();
+    let keyWord = key.toUpperCase();
+    let realKey = keyWord.repeat(Math.ceil(message.length / keyWord.length));
+    realKey = realKey.substr(0, message.length).split('');
+    
+    let encryptMessage = '';
+    for(let i = 0; i < message.length; i++) {
+      if(message.charCodeAt(i) >= 65 &&  message.charCodeAt(i) <= 90) {
+        encryptMessage = encryptMessage + String.fromCharCode(((message.charCodeAt(i) + realKey[0].charCodeAt(0)) % 26) + 65);
+        realKey.shift();
+      } else {
+        encryptMessage = encryptMessage + message.charAt(i);
+      }
+    }
+
+    if(this.type === false) {
+      return encryptMessage.split('').reverse().join('');
+    }
+    return encryptMessage;
   }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+
+  decrypt(message, key) {
+    if (message == undefined || key == undefined) {
+      throw new Error();
+    } 
+
+    message = message.toUpperCase();
+    let keyWord = key.toUpperCase();
+    let realKey = keyWord.repeat(Math.ceil(message.length / keyWord.length));
+    realKey = realKey.substr(0, message.length).split('');
+
+    let decryptMessage = '';
+    for(let i = 0; i < message.length; i++) {
+      if(message.charCodeAt(i) >= 65 &&  message.charCodeAt(i) <= 90) {
+        decryptMessage = decryptMessage + String.fromCharCode(((message.charCodeAt(i) + 26 - realKey[0].charCodeAt(0)) % 26) + 65);
+        realKey.shift();
+      } else {
+        decryptMessage = decryptMessage + message.charAt(i);
+      }
+    }
+
+    if(this.type === false) {
+      return decryptMessage.split('').reverse().join('');
+    }
+    return decryptMessage;
+
   }
 }
 
